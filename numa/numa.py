8cc8363ba3924316a05cdfdfd74080c730b9b81e
FugaTakata <maple1213goo@gmail.com>
2021-07-24 12:02:44 +0900

    add numa script

---
+++ numa/numa.py
@@ -0,0 +1,17 @@
+import json
+import csv
+
+data = None
+
+with open('trim/seedData.json') as f:
+    data = json.load(f)
+
+with open('numa/numa.csv', mode='w') as f:
+    writer = csv.writer(f)
+    writer.writerow(['年', '俳優名'])
+
+    for movie in data:
+        year = movie['releaseDate'][:4]
+        for actor in movie['cast']:
+            name = actor['name']
+            writer.writerow([year, name])
